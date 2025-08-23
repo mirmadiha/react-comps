11a011b0ec063e0accdb2677c550f022593e1eb9
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-08-23 21:03:39 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Button from './Button';
+function App(){
+    return(
+        <div>
+        <div>
+            <Button>click me!</Button>
+        </div>
+
+        <div>
+            <Button>tap here!</Button>
+        </div>
+        </div>
+    );     
+};
+
+export default App;
