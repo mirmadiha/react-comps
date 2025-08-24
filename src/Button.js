331db528e962e586fb53c920c84cce00819e1bed
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-08-24 23:19:41 +0530

    prop-types

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,13 @@
-function Button({children}){
+import PropTypes from 'prop-types';
+function Button({
+    children,primary,secondary,success,warning,danger,outline,rounded}){
     return <button>{children}</button>
 };
+
+Button.propTypes={
+    checkVariationValue:(props)=>{
+        console.log(props);
+    }
+}
 
 export default Button;
