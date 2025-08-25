5d03088af62d1cff633011f1dbd3cce71f5b4e08
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-08-25 13:35:23 +0530

    tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Button from './Button';
+import { GoBell , GoHorizontalRule   ,GoKebabHorizontal} from "react-icons/go";
 function App(){
     return(
         <div>
         <div>
-            <Button>click me!</Button>
+            <Button success rounded outline>click me! <GoBell/> </Button>
         </div>
 
         <div>
-            <Button>tap here!</Button>
+            <Button danger outline>Buy Now! <GoHorizontalRule/> </Button>
         </div>
+
+        <div>
+            <Button warning>See Deals! <GoKebabHorizontal/> </Button>
+        </div>
+
+        <div>
+            <Button secondary outline>Hide Ads!</Button>
+        </div>
+
+        <div>
+            <Button primary rounded>Something!</Button>
+        </div>
+        
         </div>
     );     
 };
--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,21 @@
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 function Button({
     children,primary,secondary,success,warning,danger,outline,rounded}){
-    return <button>{children}</button>
+        const classes=classNames("flex items-center px-3 , py-1.5 border",
+            {"border-blue-500 bg-blue-500 text-white":primary},
+            {"border-black bg-black text-white":secondary},
+            {"border-green-500 bg-green-500 text-white":success},
+            {"border-yellow-500 bg-yellow-500 text-white":warning},
+            {"border-red-500 bg-red-500 text-white":danger},
+            {"rounded-full":rounded},
+            {"text-blue-500":outline && primary},
+            {"text-black":outline && secondary},
+            {"text-green-500":outline && success},
+            {"text-yellow-500":outline && warning},
+            {"text-red-500":outline && danger},
+        )
+    return <button className={classes} >{children}</button>
 };
 
 Button.propTypes={
