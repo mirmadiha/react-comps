10816e90f8eb6c4ca96996a9cb11873f415b5181
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-08-26 19:31:51 +0530

    accordion

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ function App(){
     const items=[
         {id:"ew4564",
         label:"Js",
-         content:"Js is super fun!"
+         content:"JavaScript is the language of the web, making websites dynamic, interactive, and engaging. Unlike C and C++, it runs directly in browsers and powers everything from animations to full web applications. With frameworks like React and Node.js, JavaScript has grown far beyond the browser into a versatile, modern programming language."
         },
 
         {
          id:"fdf5",   
         label:"C",
-         content:"C is super fun!"
+         content:"C is known as the “mother of all programming languages.” It is a powerful, low-level language that gives direct control over memory and hardware. Because of its speed and efficiency, C is widely used in operating systems, embedded systems, and performance-critical applications."
         },
 
         {
         id:"ghjn88",
         label:"C++",
-         content:"C++ is super fun!"
+         content:"C++ builds on C by adding object-oriented features like classes and objects. It balances performance with abstraction, allowing developers to write fast programs while also managing complexity. From game engines to high-performance applications, C++ is a backbone of modern software development."
         }
     ]
     return(
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,29 @@
 import {useState} from "react";
+import { GoChevronDown,GoChevronLeft} from "react-icons/go";
 function Accordion({items}){
+    const[expandedIndex,setExpandedIndex]=useState(-1);
+
+    const handleClick=(nextIndex)=>{
+        if(expandedIndex===nextIndex){
+            setExpandedIndex(-1);
+        }
+        else{
+        setExpandedIndex(nextIndex);
+        }
+    }
+
     const renderedItems=items.map((item,index)=>{
-        const[expandedIndex,setExpandedIndex]=useState(0);
         const isExpanded=expandedIndex===index;
         console.log(isExpanded);
+
+        const icon=<span className="text-2xl">
+            {isExpanded?<GoChevronDown/>:<GoChevronLeft/>}
+        </span>
+
         return(
             <div key={item.id}>
-                <div>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>} 
+                <div className="flex justify-between p-3 bg-gray-100 border-b items-center cursor-pointer"onClick={()=>{handleClick(index)}}>{item.label}{icon}</div>
+                {isExpanded && <div className="border-b border-gray-200 p-3">{item.content}</div>} 
             </div>
         );
     });
